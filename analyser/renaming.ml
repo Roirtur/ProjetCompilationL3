03faced5956eb578c8218553a1e93b10501d9f58
Arthur Macdonald <arthur.macdonald@u-bordeaux.fr>
2024-04-03 14:50:24 +0200

    Fixed and finished renaming

--- analyser/renaming.ml
+++ analyser/renaming.ml
@@ -23,7 +23,7 @@ let rec renaming_expression (expression, counter_env) =
    | Variable (name, anno) -> (match (Environment.get counter_env name) with
       | None -> Variable (name, anno)
       | Some value when value = 0 -> Variable (name, anno)
-      | Some value -> Variable ((name ^ "#" ^ (string_of_int (value + 1))), anno)
+      | Some value -> Variable ((name ^ "#" ^ (string_of_int value)), anno)
    )
    | Binary_operator (op, e1, e2, anno) -> Binary_operator (op, renaming_expression (e1, counter_env), renaming_expression (e2, counter_env), anno)
    | Unary_operator (op, e, anno) -> Unary_operator (op, renaming_expression (e, counter_env), anno)
@@ -38,15 +38,19 @@ let rec renaming_statement (statement, counter_env) =
       | None -> Environment.add counter_env id 0; Declaration(id, typ, anno)
       | Some value -> Environment.modify counter_env id (value + 1); Declaration((id ^ "#" ^ (string_of_int (value + 1))), typ, anno)
    )
-   | Block (block, anno) -> Block (List.map (fun x -> renaming_statement (x, Environment.copy counter_env)) block, anno)
+   | Block (block, anno) -> let new_env = Environment.copy counter_env in Block (List.map (fun x -> renaming_statement (x, new_env)) block, anno)
    | IfThenElse (condition, then_block, else_block, anno) -> IfThenElse (condition, renaming_statement (then_block, Environment.copy counter_env), renaming_statement (else_block, Environment.copy counter_env), anno)
-   | For (id, start, stop, step, block, anno) -> (match (Environment.get counter_env id) with
-      | None -> Environment.add counter_env id 0; For(id, start, stop, step, renaming_statement (block, Environment.copy counter_env), anno)
-      | Some value -> Environment.modify counter_env id (value + 1); For((id ^ "#" ^ (string_of_int (value + 1))), start, stop, step, renaming_statement (block, Environment.copy counter_env), anno)
+   | For (id, start, stop, step, block, anno) -> 
+      let new_start = renaming_expression (start, counter_env) in
+      let new_stop = renaming_expression (stop, counter_env) in
+      let new_step = renaming_expression (step, counter_env) in
+      let new_env = Environment.copy counter_env in (match (Environment.get new_env id) with
+      | None -> (Environment.add new_env id 0; For(id, new_start, new_stop, new_step, renaming_statement (block, new_env), anno))
+      | Some value -> (Environment.modify new_env id (value + 1); For((id ^ "#" ^ (string_of_int (value + 1))), new_start, new_stop, new_step, renaming_statement (block, new_env), anno))
    )
-   | Foreach (id, expression, block, anno) -> (match (Environment.get counter_env id) with
-      | None -> Environment.add counter_env id 0; Foreach(id, renaming_expression (expression, counter_env), renaming_statement (block, Environment.copy counter_env), anno)
-      | Some value -> Environment.modify counter_env id (value + 1); Foreach((id ^ "#" ^ (string_of_int (value + 1))), renaming_expression (expression, counter_env), renaming_statement (block, Environment.copy counter_env), anno)
+   | Foreach (id, expression, block, anno) -> let new_env = Environment.copy counter_env in (match (Environment.get new_env id) with
+      | None -> Environment.add new_env id 0; Foreach(id, renaming_expression (expression, counter_env), renaming_statement (block, new_env), anno)
+      | Some value -> Environment.modify new_env id (value + 1); Foreach((id ^ "#" ^ (string_of_int (value + 1))), renaming_expression (expression, counter_env), renaming_statement (block, new_env), anno)
    )
    | Draw_pixel (expression, anno) -> Draw_pixel (renaming_expression (expression, counter_env), anno)
    | Nop -> Nop
@@ -55,8 +59,11 @@ let rec renaming_statement (statement, counter_env) =
 
 let renaming (program) =
    let name_counter = Environment.new_environment () in
-   let new_prog = (match program with
-    | Program (arg_list, statement) -> Program (renaming_arg_list (arg_list, name_counter), renaming_statement (statement, name_counter)))
-   in new_prog
+   match program with
+    | Program (arg_list, statement) -> (
+      let new_arg_list = renaming_arg_list (arg_list, name_counter) in
+      let new_statement = renaming_statement (statement, name_counter) in
+      Program (new_arg_list, new_statement)
+      )
 
  
